a633f8625d7ab0eaa916b58350fcc7ae66a3b925
djberg96 <djberg96>
2006-01-06 19:09:14 +0000

    Lots of refactoring and additional comments Fixed bug for Row#[] where multiple non integers were not handled properly Fixed bug where Ranges with excluded ends were accidentally being included

--- lib/dbi/row.rb
+++ lib/dbi/row.rb
@@ -1,157 +1,194 @@
-#
-# $Id: row.rb,v 1.1 2006/01/04 02:03:22 francis Exp $
-#
-
 require "delegate"
 
-
-# TODO: a method which returns the column-names,
-#       #columns or #names ? 
-
-
+# The DBI::Row class is a delegate of Array, rather than a subclass, because
+# there are times when it should act like an Array, and others when it should
+# act like a Hash (and still others where it should act like String, Regexp,
+# etc).  It also needs to store metadata about the row, such as
+# column data type and index information, that users can then access.
+#
 module DBI
+   class Row < DelegateClass(Array)
+      attr_reader :column_names
 
-class Row < DelegateClass(Array)
+      # DBI::Row.new(columns, size_or_array=nil)
+      #
+      # Returns a new DBI::Row object using +columns+.  The +size_or_array+
+      # argument may either be an Integer or an Array.  If it is not provided,
+      # it defaults to the length of +columns+.
+      #
+      def initialize(columns, size_or_array=nil)
+         size_or_array ||= columns.size 
 
-  include Enumerable
+         case size_or_array
+            when Integer
+               @arr = Array.new(size_or_array)
+            when Array
+               @arr = size_or_array
+            else
+               raise TypeError, "parameter must be either Integer or Array"
+         end
 
-  def initialize(col_names, size_or_arr=nil)
-    size_or_arr ||= col_names.size 
+         # The '@column_map' is used to map column names to integer values so
+         # that users can reference row values by name or number.
 
-    if size_or_arr.is_a? Integer
-      @arr = Array.new(size_or_arr)
-    elsif size_or_arr.is_a? Array
-      @arr = size_or_arr
-   else
-      raise ArgumentError, "parameter must be either Integer or Array"   
-    end
+         @column_map   = {}
+         @column_names = columns
+         columns.each_with_index { |c,i| @column_map[c] = i }
+         super(@arr)
+      end
 
-    @col_map = {}
-    @col_names = col_names
-    col_names.each_with_index {|c,i| @col_map[c] = i}
-    super(@arr)
-  end
+      # Replaces the contents of @arr with +new_values+
+      def set_values(new_values)
+         @arr.replace(new_values)
+      end
+      
+      # Yields a column value by name (rather than index).
+      def each_with_name
+         @arr.each_with_index do |v, i|
+            yield v, @column_names[i]
+         end 
+      end
+      
+      # Returns the Row object as a hash
+      def to_h
+         hash = {}
+         each_with_name{ |v, n| hash[n] = v}
+         hash
+      end
+      
+      # Create a new row with 'new_values', reusing the field name hash.
+      def clone_with(new_values)    
+         Row.new(@column_names, new_values)
+      end
 
-  # Modifiers --------------------------------------------------------
+      def []=(*args)
+         #if args.size == 1
+         #  raise ArgumentError, "wrong # of arguments(#{args.size} for at least 2)"
+         if args.size == 2
+            @arr[conv_param(args[0])] = args[-1]
+         elsif args.size == 3
+            @arr[conv_param(args[0]), conv_param(args[1])] = args[-1]
+         else
+            raise ArgumentError, "wrong # of arguments(#{args.size} for 2)"
+         end
+      end
 
-  def set_values(new_values)
-    # Accept an array of new values
-    new_values.each_with_index do |v,i|
-      @arr[i] = v
-    end
-  end
+      alias field_names column_names
 
-  def []=(*args)
-     #if args.size == 1
-     #  raise ArgumentError, "wrong # of arguments(#{args.size} for at least 2)"
-     if args.size == 2
-       @arr[conv_param(args[0])] = args[-1]
-     elsif args.size == 3
-       @arr[conv_param(args[0]), conv_param(args[1])] = args[-1]
-     else
-       raise ArgumentError, "wrong # of arguments(#{args.size} for 2)"
-     end
-  end
-
-  # Cloning and Conversion -------------------------------------------
+      # Retrieve a value by index (rather than name)
+      def by_index(index)
+         @arr[index]
+      end
     
-  def to_h
-    hash = {}
-    each_with_name {|v, n| hash[n] = v}
-    hash
-  end
-
+      # Value of the field named +field_name+ or nil if not found.
+      def by_field(field_name)
+         begin
+            @arr[@column_map[field_name.to_s]]
+         rescue TypeError
+            nil
+         end
+      end
  
-  def clone_with(new_values)
-    # Create a new row with 'new_values', reusing the field name hash. 
-    Row.new(@col_names, new_values)
-  end
- 
-  # Queries ----------------------------------------------------------
-
-  def column_names
-    @col_names
-  end
-
-  alias field_names column_names
-
-  def by_index(index)
-    # Value at 'index'.
-    @arr[index]
-  end
-    
-  def by_field(field_name)
-    # Value of the field named 'field_name'.
-    @arr[@col_map[field_name.to_s]]
-  rescue TypeError
-    nil
-  end
- 
-  def [](*args)
-     if args.size == 0
-       raise ArgumentError, "wrong # of arguments(#{args.size} for at least 1)"
-       # what todo if no param? => .to_a? .dup?
-     elsif args.size == 1
-       if args[0].is_a? Array
-         args[0].collect {|i| self[i]}
-       elsif args[0].is_a? Regexp
-         cols = @col_names.grep args[0] 
-         self[cols]
-       else
-         @arr[conv_param(args[0])]
-       end
-     elsif args.size == 2
-       @arr[conv_param(args[0]), conv_param(args[1])]
-     else
-       args.collect {|i| self[i]}
-       #raise ArgumentError, "wrong # of arguments(#{args.size} for 2)"
-     end
-  rescue TypeError
-    nil
-  end
-
-  def each_with_name
-    @arr.each_with_index do |v, i|
-      yield v, @col_names[i]
-    end 
-  end
-
-  def clone
-    clone_with(@arr.dup)
-  end
-
-  alias dup clone
-
-  private # ----------------------------------------------------------
-
-  def conv_param(p)
-    if p.is_a? String or p.is_a? Symbol then
-      @col_map[p.to_s]
-    elsif p.is_a? Range then
-
-      if p.first.is_a? String or p.first.is_a? Symbol then
-        first = @col_map[p.first.to_s]
-      else
-        first = p.first
+      # Row#[]
+      #
+      # row[int]
+      # row[array]
+      # row[regexp]
+      # row[arg, arg]
+      # row[arg, arg, ...]
+      #
+      # Sample: Row.new(["first","last","age"], ["Daniel", "Berger", "36"])
+      #
+      # Retrieves row elements.  Exactly what it retrieves depends on the
+      # kind and number of arguments used.
+      #
+      # Zero arguments will raise an ArgumentError.
+      #
+      # One argument will return a single result.  This can be a String,
+      # Symbol, Integer, Range or Regexp and the appropriate result will
+      # be returned.  Strings, Symbols and Regexps act like hash lookups,
+      # while Integers and Ranges act like Array index lookups.
+      #
+      # Two arguments will act like the second form of Array#[], i.e it takes
+      # two integers, with the first number the starting point and the second
+      # number the length, and returns an array of values.
+      #
+      # If three or more arguments are provided, an array of results is
+      # returned.  The behavior for each argument is that of a single argument,
+      # i.e. Strings, Symbols, and Regexps act like hash lookups, while
+      # Integers and Ranges act like Array index lookups.
+      # 
+      # If no results are found, or an unhandled type is passed, then nil
+      # (or a nil element) is returned.
+      #
+      def [](*args)
+         begin
+            case args.length
+               when 0
+                  err = "wrong # of arguments(#{args.size} for at least 1)"
+                  raise ArgumentError, err
+               when 1
+                  case args[0]
+                     when Array
+                        args[0].collect { |e| self[e] }
+                     when Regexp
+                        self[@column_names.grep(args[0])] 
+                     else
+                        @arr[conv_param(args[0])]
+                  end
+               # We explicitly check for a length of 2 in order to properly
+               # simulate the second form of Array#[].
+               when 2
+                  @arr[conv_param(args[0]), conv_param(args[1])]
+               else
+                  results = []
+                  args.flatten.each{ |arg|
+                     case arg
+                        when Integer
+                           results.push(@arr[arg])
+                        when Regexp
+                           results.push(self[@column_names.grep(arg)])
+                        else
+                           results.push(self[conv_param(arg)])
+                     end
+                  }
+                  results.flatten
+            end
+         rescue TypeError
+            nil
+         end
       end
 
-      if p.last.is_a? String or p.last.is_a? Symbol then
-        last = @col_map[p.last.to_s]
-      else
-        last = p.last
+      def clone
+         clone_with(@arr.dup)
       end
 
-      if p.exclude_end?
-        (first..last)
-      else
-        (first..last)
+      alias dup clone
+
+      private
+
+      # Simple helper method to grab the proper value from @column_map
+      def conv_param(arg)
+         case arg
+            when String, Symbol
+               @column_map[arg.to_s]
+            when Range
+               if arg.first.kind_of?(Symbol) || arg.first.kind_of?(String)
+                  first = @column_map[arg.first.to_s]
+                  last  = @column_map[arg.last.to_s]
+               else
+                  first = @column_map[arg.first]
+                  last  = @column_map[arg.last]
+               end
+
+               if arg.exclude_end?
+                  (first...last) 
+               else
+                  (first..last)
+               end
+            else
+               arg
+         end
       end
-
-    else
-      p
-    end
-  end
-
-end # class Row
-
+   end # class Row
 end # module DBI
