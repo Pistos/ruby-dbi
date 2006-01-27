1ffe8cc1a82691e15e6e86c07206a441642c013c
djberg96 <djberg96>
2006-01-27 23:28:47 +0000

    Added tests to verify that attr values could be accessed as a hash key (string or symbol) or by method

--- test/dbi/tc_columninfo.rb
+++ test/dbi/tc_columninfo.rb
@@ -14,25 +14,54 @@ require "test/unit"
 
 class TC_DBI_ColumnInfo < Test::Unit::TestCase
    def setup
-      @colinfo = ColumnInfo.new
+      @colinfo = ColumnInfo.new(
+         "name"      => "test",
+         "sql_type"  => "numeric",
+         "type_name" => "test_type_name",
+         "precision" => 2,
+         "scale"     => 2,
+         "default"   => 100.00,
+         "nullable"  => false,
+         "indexed"   => true,
+         "primary"   => true,
+         "unique"    => false
+      )
    end
 
-   def test_name
+   def test_name_basic
       assert_respond_to(@colinfo, :name)
       assert_respond_to(@colinfo, :name=)
    end
 
-   def test_sql_type
+   def test_name
+      assert_equal("test", @colinfo[:name])
+      assert_equal("test", @colinfo["name"])
+      assert_equal("test", @colinfo.name)
+   end
+
+   def test_sql_type_basic
       assert_respond_to(@colinfo, :sql_type)
       assert_respond_to(@colinfo, :sql_type=)
    end
+   
+   def test_sql_type
+      assert_equal("numeric", @colinfo[:sql_type])
+      assert_equal("numeric", @colinfo["sql_type"])
+      assert_equal("numeric", @colinfo.sql_type)
+   end
 
-   def test_type_name
+   def test_type_name_basic
       assert_respond_to(@colinfo, :type_name)
       assert_respond_to(@colinfo, :type_name=)
    end
 
-   def test_precision
+   def test_type_name
+      assert_equal("test_type_name", @colinfo[:type_name])
+      assert_equal("test_type_name", @colinfo["type_name"])
+      assert_equal("test_type_name", @colinfo.type_name)
+   end
+
+   def test_precision_basic
       assert_respond_to(@colinfo, :precision)
       assert_respond_to(@colinfo, :precision=)
       assert_respond_to(@colinfo, :size)     
@@ -41,41 +70,83 @@ class TC_DBI_ColumnInfo < Test::Unit::TestCase
       assert_respond_to(@colinfo, :length=)
    end
 
-   def test_scale
+   def test_precision
+      assert_equal(2, @colinfo[:precision])
+      assert_equal(2, @colinfo[:precision])
+      assert_equal(2, @colinfo.precision)
+   end
+
+   def test_scale_basic
       assert_respond_to(@colinfo, :scale)
       assert_respond_to(@colinfo, :scale=)
       assert_respond_to(@colinfo, :decimal_digits)
       assert_respond_to(@colinfo, :decimal_digits=)
    end
 
-   def test_default
+   def test_scale
+      assert_equal(2, @colinfo[:scale])
+      assert_equal(2, @colinfo["scale"])
+      assert_equal(2, @colinfo.scale)
+   end
+
+   def test_default_basic
       assert_respond_to(@colinfo, :default)
       assert_respond_to(@colinfo, :default=)
    end
 
-   def test_nullable
+   def test_default
+      assert_equal(100.00, @colinfo[:default])
+      assert_equal(100.00, @colinfo["default"])
+      assert_equal(100.00, @colinfo.default)
+   end
+
+   def test_nullable_basic
       assert_respond_to(@colinfo, :nullable)
       assert_respond_to(@colinfo, :nullable?)
       assert_respond_to(@colinfo, :nullable=)
    end
 
-   def test_indexed
+   def test_nullable
+      assert_equal(false, @colinfo[:nullable])
+      assert_equal(false, @colinfo["nullable"])
+      assert_equal(false, @colinfo.nullable)
+   end
+
+   def test_indexed_basic
       assert_respond_to(@colinfo, :indexed)
       assert_respond_to(@colinfo, :indexed?)
       assert_respond_to(@colinfo, :indexed=)
    end
 
-   def test_primary
+   def test_indexed
+      assert_equal(true, @colinfo[:indexed])
+      assert_equal(true, @colinfo["indexed"])
+      assert_equal(true, @colinfo.indexed)
+   end
+
+   def test_primary_basic
       assert_respond_to(@colinfo, :primary)
       assert_respond_to(@colinfo, :primary?)
       assert_respond_to(@colinfo, :primary=)
    end
 
-   def test_unique
+   def test_primary
+      assert_equal(true, @colinfo[:primary])
+      assert_equal(true, @colinfo["primary"])
+      assert_equal(true, @colinfo.primary)
+   end
+
+   def test_unique_basic
       assert_respond_to(@colinfo, :unique)
       assert_respond_to(@colinfo, :unique?)
       assert_respond_to(@colinfo, :is_unique)
       assert_respond_to(@colinfo, :unique=)
+   end
+
+   def test_unique
+      assert_equal(false, @colinfo[:unique])
+      assert_equal(false, @colinfo["unique"])
+      assert_equal(false, @colinfo.unique)
    end
 
    def teardown
