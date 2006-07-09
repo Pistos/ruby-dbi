581ee3bf46588a86acb3d71bbbae478456f0dd59
djberg96 <djberg96>
2006-07-09 16:21:01 +0000

    Removed hardcoded search paths to search path Modified dir_config name to lower case

--- ext/dbd_sqlite/extconf.rb
+++ ext/dbd_sqlite/extconf.rb
@@ -1,7 +1,7 @@
-require "mkmf"
+require 'mkmf'
 
-dir_config "SQLite", "/usr/local"
+dir_config 'sqlite'
 
-if find_library("sqlite", "sqlite_open", "/usr/local/lib", "/usr/pkg/lib") and have_header("sqlite.h")
-  create_makefile "SQLite"
+if find_library('sqlite', 'sqlite_open') and have_header('sqlite.h')
+   create_makefile 'SQLite'
 end
