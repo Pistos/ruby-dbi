c44e1341558f9fa458b66d222ff227338610b336
Erik Hollensbe <erikh@speyside.(none)>
2008-02-23 16:43:19 -0800

    * Not quite the same, but an automated ping test that tests that's it's throwing happy little exceptions.

--- test/dbd/postgresql/test_ping.rb
+++ test/dbd/postgresql/test_ping.rb
@@ -1,8 +1,17 @@
-require "dbi"
+require 'test/unit'
 
-DBI.connect("dbi:Pg:michael:127.0.0.1", "michael", "michael") do |dbh|
-  p dbh.ping
-  puts "Shut now Pg down"; $stdin.readline
-  p dbh.ping
+class TestPostgresPing < Test::Unit::TestCase
+    def test_ping
+        dbh = DBI.connect("dbi:Pg:rubytest:127.0.0.1", "erikh", "monkeys")
+        assert dbh
+        assert dbh.ping
+        dbh.disconnect
+        assert_raise(DBI::InterfaceError) { dbh.ping }
+    end
 end
 
+if __FILE__ == $0 then
+    require 'test/unit/ui/console/testrunner'
+    require 'dbi'
+    Test::Unit::UI::Console::TestRunner.run(TestPostgresPing)
+end
