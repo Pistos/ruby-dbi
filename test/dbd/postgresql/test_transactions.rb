2ec978106286c0cc2022ff5f5427e671df14c06c
Erik Hollensbe <erikh@speyside.(none)>
2008-02-24 12:32:14 -0800

    * commit/rollback tests for Pg

--- test/dbd/postgresql/test_transactions.rb
+++ test/dbd/postgresql/test_transactions.rb
@@ -1,6 +1,51 @@
 require 'test/unit'
 
 class TestPostgresTransaction < Test::Unit::TestCase
+    def test_rollback
+        dbh = get_dbh
+        dbh["AutoCommit"] = false
+        sth = dbh.prepare('insert into names (name, age) values (?, ?)')
+        sth.execute("Foo", 51)
+        dbh.rollback
+        assert_equal 1, sth.rows
+        sth.finish
+
+
+        sth = dbh.prepare('select name, age from names where name=?')
+        sth.execute("Foo")
+        assert !sth.fetch
+        sth.finish
+    end
+
+    def test_commit
+        dbh = get_dbh
+        dbh["AutoCommit"] = false
+        sth = dbh.prepare('insert into names (name, age) values (?, ?)')
+        sth.execute("Foo", 51)
+        dbh.commit
+        assert_equal 1, sth.rows
+        sth.finish
+        
+        sth = dbh.prepare('select name, age from names where name=?')
+        sth.execute("Foo")
+        row = sth.fetch
+        assert row
+        assert_equal "Foo", row[0]
+        assert_equal 51, row[1]
+        sth.finish
+    end
+
+    def get_dbh
+        DBI.connect('dbi:Pg:rubytest', 'erikh', 'monkeys')
+    end
+
+    def setup
+        system "psql rubytest < dump.sql >>sql.log"
+    end
+
+    def teardown
+        system "psql rubytest < drop_tables.sql >>sql.log"
+    end
 end
 
 if __FILE__ == $0 then
