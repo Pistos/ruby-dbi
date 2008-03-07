7f5e653ab52bfbf02d37ba6e25ffdf37e9ceb48c
Erik Hollensbe <erikh@speyside.(none)>
2008-03-06 20:05:07 -0800

    * added a 'command' config line to the mysql config for starting with different command names

--- test/dbd/mysql/base.rb
+++ test/dbd/mysql/base.rb
@@ -5,14 +5,14 @@ DBDConfig.set_testbase(:mysql, Class.new(Test::Unit::TestCase) do
 
         def setup
             config = DBDConfig.get_config["mysql"]
-            system("mysql5 -u #{config['username']} #{config['password'] ? "-p "+config['password'] : ''} #{config['dbname']} < dbd/mysql/up.sql")
-            @dbh = DBI.connect("dbi:mysql:"+config["dbname"], config["username"], config["password"], { })
+            system("#{config['command'] || "mysql"} -u #{config['username']} #{config['password'] ? "-p "+config['password'] : ''} #{config['dbname']} < dbd/mysql/up.sql")
+            @dbh = DBI.connect("dbi:Mysql:"+config["dbname"], config["username"], config["password"], { })
         end
 
         def teardown
             @dbh.disconnect
             config = DBDConfig.get_config["mysql"]
-            system("mysql5 -u #{config['username']} #{config['password'] ? "-p "+config['password'] : ''} #{config['dbname']} < dbd/mysql/down.sql")
+            system("#{config['command'] || "mysql"} -u #{config['username']} #{config['password'] ? "-p "+config['password'] : ''} #{config['dbname']} < dbd/mysql/down.sql")
         end
     end
 )
