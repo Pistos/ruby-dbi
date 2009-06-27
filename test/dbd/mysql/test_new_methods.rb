eb624db1f99154e2aade9ed124e2bc89425573ac
Erik Hollensbe <erikh@erikh-desktop.(none)>
2009-06-27 03:36:44 -0700

    * Fixed tests; they now properly key off the expected databaase names in the DBD test config files.

--- test/dbd/mysql/test_new_methods.rb
+++ test/dbd/mysql/test_new_methods.rb
@@ -1,7 +1,7 @@
 class TestNewMethods < DBDConfig.testbase(:mysql)
     def test_database_name
         assert_nothing_raised do
-            assert_equal "rubytest", @dbh.database_name
+            assert_equal DBDConfig.get_config[dbtype]['dbname'], @dbh.database_name
         end
     end
 end
--- test/dbd/sqlite3/test_database.rb
+++ test/dbd/sqlite3/test_database.rb
@@ -1,7 +1,7 @@
 class TestDatabase < DBDConfig.testbase(:sqlite3)
     def test_database_name
         assert_nothing_raised do
-            assert_equal "/tmp/foo.db", @dbh.database_name
+            assert_equal DBDConfig.get_config[dbtype]['dbname'], @dbh.database_name
         end
     end
 
