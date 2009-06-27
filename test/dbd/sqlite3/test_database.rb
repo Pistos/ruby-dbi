df15530712c7d99774c915d077037e1ce6cd201f
Erik Hollensbe <erikh@erikh-desktop.(none)>
2009-06-27 03:27:30 -0700

    * database_name for SQLite3

--- test/dbd/sqlite3/test_database.rb
+++ test/dbd/sqlite3/test_database.rb
@@ -1,4 +1,10 @@
 class TestDatabase < DBDConfig.testbase(:sqlite3)
+    def test_database_name
+        assert_nothing_raised do
+            assert_equal "/tmp/foo.db", @dbh.database_name
+        end
+    end
+
     def test_disconnect
         assert_nil @dbh.disconnect
         assert_nil @dbh.instance_variable_get("@db")
